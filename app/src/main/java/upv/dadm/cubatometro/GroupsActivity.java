230752eba6d8b490f312ba56f5acfe02d10e0cd2
andrei22a <andreicarlet2@gmail.com>
2020-03-28 16:51:28 +0100

    layout group, creategroup, ranking

--- app/src/main/java/upv/dadm/cubatometro/GroupsActivity.java
+++ app/src/main/java/upv/dadm/cubatometro/GroupsActivity.java
@@ -1,9 +1,14 @@
 package upv.dadm.cubatometro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.cubatometro.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class GroupsActivity extends AppCompatActivity {
 
@@ -11,5 +16,29 @@ public class GroupsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview_groups);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_create_group:
+                startActivity(new Intent(GroupsActivity.this, CreateGroupActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
     }
 }
--- app/src/main/java/upv/dadm/cubatometro/LoginActivity.java
+++ app/src/main/java/upv/dadm/cubatometro/LoginActivity.java
@@ -1,7 +1,11 @@
 package upv.dadm.cubatometro;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity{
 
@@ -9,5 +13,14 @@ public class LoginActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.inicio_button_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, GroupsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
